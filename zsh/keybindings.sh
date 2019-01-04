15938da5b6360bef9868c67842c9af537e903838
Seth Lowie <slowie@powerchord.com>
2019-01-04 14:15:02 -0500

    change tmux prefix

--- zsh/keybindings.sh
+++ zsh/keybindings.sh
@@ -1,5 +1,5 @@
 # git stuff
-function git_commit_push{
+function git_commit_push(){
 	if [ -n "$BUFFER" ]; then
 		BUFFER="git add -A && git commit -m \"$BUFFER\" && git push"
 	fi
@@ -11,4 +11,4 @@ function git_commit_push{
 	zle accept-line
 }
 zle -N git_commit_push
-bindkey "^g" git_fancy_commit
+bindkey "^g" git_commit_push
