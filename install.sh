388d35d48a85942cd994f2c7fa9bda87a806b373
Seth Lowie <slowie@powerchordsystem.com>
2019-03-21 12:04:18 -0400

    created install script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+sudo apt-get update
+
+########################################
+############## VIM SETUP ###############
+########################################
+
+# Install vim-plug
+# https://github.com/junegunn/vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install pip3
+sudo apt-get install -y python3-pip
+
+# Install neovim library for vim deoplete
+pip3 install neovim
+
+echo "source ~/dotfiles/vim/vimrc.vim" >> ".vimrc"
+
+
+########################################
+############## TMUX SETUP ##############
+########################################
+
+sudo apt-get install -y tmux
+
+echo "source ~/dotfiles/tmux/tmux.conf" >> ".tmux.conf"
+
+########################################
+############## ZSH SETUP ###############
+########################################
+
+echo "source ~/dotfiles/zsh/zshrc_manager.sh" >> ".zshrc"
+
