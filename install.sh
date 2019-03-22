c268a04977992e7eeff8417cf0ac562843d7b48d
Seth Lowie <slowie@powerchordsystem.com>
2019-03-22 17:20:24 -0400

    updated install script to ask for user info

--- install.sh
+++ install.sh
@@ -1,7 +1,40 @@
 #! /bin/bash
 
-echo "Running the setup"
+echo "Name:"
+read name
 
-# sudo apt-get update
+echo "Email:"
+read email
 
+sudo apt-get update
+
+sudo apt-get install -y git
+
+git config --global user.name "$name"
+git config --global user.email "$email"
+
+ssh-keygen -t rsa -b 4096 -C "$email" -P "" -f ~/.ssh/my_key
+
+## EXTRAS
+# sh ./scripts/haskell.sh
+
+echo ""
+echo "## COPY THIS PUBLIC KEY TO GITHUB ##"
+echo ""
+echo ~/.ssh/my_rsa.pub
+echo ""
+echo ""
+
+read -p "Press Enter to Continue Installation"
+
+git clone git@github.com:logiXbomb/dotfiles.git ~/dotfiles
+
+sh ./scripts/vim.sh
+sh ./scripts/zsh.sh
+sh ./scripts/tmux.sh
+sh ./scripts/docker.sh
+sh ./scripts/docker-compose.sh
+sh ./scripts/gcloud.sh
+sh ./scripts/kubectl.sh
+sh ./scripts/go.sh
 
