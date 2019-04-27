64e213e3f4a592fab12d215a65dae2ee9bd2e566
Git <slowie@powerchordsystem.com>
2019-04-27 18:08:20 -0400

    don't script install dynamic top bar

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -8,9 +8,9 @@ gsettings set org.gnome.desktop.wm.preferences button-layout ''
 # Swap Caps Lock and Escape
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:swapescape']"
 
-cd /tmp
+# cd /tmp
 
-wget https://extensions.gnome.org/extension-data/dynamic-panel-transparencyrockon999.github.io.v29.shell-extension.zip
+# wget https://extensions.gnome.org/extension-data/dynamic-panel-transparencyrockon999.github.io.v29.shell-extension.zip
 
-unzip dynamic-panel-transparencyrockon999.github.io.v29.shell-extension.zip -d $HOME/.local/share/gnome-shell/extensions/
+# unzip dynamic-panel-transparencyrockon999.github.io.v29.shell-extension.zip -d $HOME/.local/share/gnome-shell/extensions/
 
