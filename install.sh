3c6db92ed18edbd46c7dc08cecdf8f4135d4bcdb
Git <slowie@powerchordsystem.com>
2019-04-27 13:43:50 -0400

    full setup

--- install.sh
+++ install.sh
@@ -115,5 +115,13 @@ chsh -s $(which zsh)
 
 vim +'PlugInstall --sync' +qa
 
+git clone git@github.com:logiXbomb/workfiles.git $HOME/workfiles
+
+sh $HOME/workfiles/setup.sh
+
+sh $HOME/dotfiles/scripts/kubectl.sh
+
+sh $HOME/dotfiles/scripts/gcloud.sh
+
 sudo shutdown -r now
 
--- scripts/nerd-fonts.sh
+++ scripts/nerd-fonts.sh
@@ -2,11 +2,23 @@
 
 cd /tmp
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/FiraMono.zip
+# font="FiraCode"
+# font="FiraMono"
+# font="DejaVuSansMono"
+font="Iosevka"
+# font="FiraCode"
+# font="FiraCode"
+# font="FiraCode"
+# font="FiraCode"
+# font="FiraCode"
+
+https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Iosevka.zip
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/$font.zip
 
 mkdir -p $HOME/.local/share/fonts/NerdFonts
 
-unzip FiraMono.zip -d ~/.local/share/fonts/NerdFonts
+unzip $font.zip -d ~/.local/share/fonts/NerdFonts
 
 fc-cache -f -v
 
