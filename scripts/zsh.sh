e79cf75c677e1f88d33e69630991f7ec6cd40b6a
Git <slowie@powerchordsystem.com>
2019-05-18 09:53:09 -0400

    properly install powerlevel10k

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y zsh
 sh ~/dotfiles/scripts/oh-my-zsh.sh
 
 git clone https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> $HOME/.zshrc
 
 echo "source ~/dotfiles/zsh/zshrc_manager.sh" >> $HOME/.zshrc
 
