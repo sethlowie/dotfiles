02a956014eba9933a706b072838bc978542ae046
Git <slowie@powerchordsystem.com>
2019-07-04 14:24:00 -0400

    try alacritty direct install

--- scripts/alacritty.sh
+++ scripts/alacritty.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 
-sudo wget -O /usr/local/bin/alacritty https://sync-tool.sfo2.cdn.digitaloceanspaces.com/alacritty
+# sudo wget -O /usr/local/bin/alacritty https://sync-tool.sfo2.cdn.digitaloceanspaces.com/alacritty
 
-sudo chmod +x /usr/local/bin/alacritty
+# sudo chmod +x /usr/local/bin/alacritty
 
-sudo wget -O /tmp/alacritty.desktop https://sync-tool.sfo2.cdn.digitaloceanspaces.com/alacritty.desktop
+# sudo wget -O /tmp/alacritty.desktop https://sync-tool.sfo2.cdn.digitaloceanspaces.com/alacritty.desktop
 
-sudo desktop-file-install /tmp/alacritty.desktop
+# sudo desktop-file-install /tmp/alacritty.desktop
 
-sudo update-desktop-database
+# sudo update-desktop-database
 
-sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/alacritty 50
+# sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/alacritty 50
+
+sudo apt-get install -y alacritty
 
