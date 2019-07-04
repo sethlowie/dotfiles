7b33c473b32a9ace700a2f09e2651053299ba3b5
Git <slowie@powerchordsystem.com>
2019-07-04 16:05:34 -0400

    update kubectl install

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ git clone git@github.com:logiXbomb/workfiles.git $HOME/workfiles
 
 sh $HOME/workfiles/setup.sh
 
-sh $HOME/dotfiles/scripts/kubectl.sh
-
 sh $HOME/dotfiles/scripts/gcloud.sh
 
+sh $HOME/dotfiles/scripts/kubectl.sh
