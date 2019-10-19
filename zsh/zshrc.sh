a10788861c02660b502ffac6bf614485c98e8808
Seth Lowie <seth.lowie@gmail.com>
2019-10-19 13:00:30 -0400

    updates for mac

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -12,19 +12,19 @@ case "$HOST" in
 		;;
 esac
 
-shortKubeContext() {
-	case "$(kubectl config current-context)" in
-		"gke_fusion-dev-163815_us-east1-b_tonka-dev-cluster")
-			echo "dev"
-			;;
-		"gke_platform-prod-156220_us-east1-d_tonka-production-cluster")
-			echo "prod"
-			;;
-    "gke_powerchord-dx_us-east1-b_slowie")
-      echo "workstation"
-      ;;
-	esac
-}
+# shortKubeContext() {
+# 	case "$(kubectl config current-context)" in
+# 		"gke_fusion-dev-163815_us-east1-b_tonka-dev-cluster")
+# 			echo "dev"
+# 			;;
+# 		"gke_platform-prod-156220_us-east1-d_tonka-production-cluster")
+# 			echo "prod"
+# 			;;
+#     "gke_powerchord-dx_us-east1-b_slowie")
+#       echo "workstation"
+#       ;;
+# 	esac
+# }
 
 alias run-elm="elm-live src/Main.elm --open -- --output=elm.js --debug"
 
@@ -32,11 +32,11 @@ POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
 POWERLEVEL9K_PROMPT_ON_NEWLINE=true
 POWERLEVEL9K_CUSTOM_HOST="echo $HOST"
 POWERLEVEL9K_CUSTOM_HOST_BACKGROUND="$chColor"
-POWERLEVEL9K_CUSTOM_KUBECONTEXT="shortKubeContext"
+# POWERLEVEL9K_CUSTOM_KUBECONTEXT="shortKubeContext"
 POWERLEVEL9K_CUSTOM_KUBECONTEXT_BACKGROUND="red"
 
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-	custom_kubecontext
+	# custom_kubecontext
 	dir
 	vcs
 )
@@ -47,7 +47,8 @@ POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
 )
 
 # GO STUFF
-export GOROOT=/usr/local/go
+# export GOROOT=/usr/local/bin/go
+export GOROOT="$(brew --prefix golang)/libexec"
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
