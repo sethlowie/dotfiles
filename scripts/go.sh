e8061660c5785f42ff1580bc79c1bd92af52f3ce
Seth Lowie <seth.lowie@couchbase.com>
2020-08-20 05:40:54 -0400

    update go install script

--- scripts/go.sh
+++ scripts/go.sh
@@ -3,10 +3,12 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 
-GO_VERSION=1.13
+GO_VERSION=1.15
 
 cd /tmp
 wget "https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz"
+wget "https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz"
+
 
 sudo tar -xvf "go$GO_VERSION.linux-amd64.tar.gz"
 sudo mv go /usr/local
