65feb5b0f89c3b2baa9b9faf247745ecd38d799e
Seth Lowie <seth.lowie@gmail.com>
2021-04-20 07:34:33 -0400

    Update vim.sh

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -2,14 +2,14 @@
 
 # Install vim-plug
 # https://github.com/junegunn/vim-plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Install pip3
-sudo apt-get install -y neovim python-neovim python3-neovim
+sudo apt-get install -y neovim python3-neovim
 
 # Install neovim library for vim deoplete
-pip3 install neovim
+# pip3 install neovim
 
 mkdir -p $HOME/.config/nvim
 
