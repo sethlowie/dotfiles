e4bc333b54fc5de4194b1cfd47521408122ff86b
Seth Lowie <seth.lowie@couchbase.com>
2023-01-10 14:13:38 -0500

    update git diff

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,1 +1,6 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set('n', '<leader>gs', ':DiffviewOpen<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gc', ':botright Git commit<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gp', ':Git push -v<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gg', ':45wincmd_<CR>', { noremap = true, silent = true })
+
+
