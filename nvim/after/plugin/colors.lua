50c28304901b5230ae4dc0611ab0aa62f7a92788
Seth Lowie <seth.lowie@couchbase.com>
2023-01-10 14:08:50 -0500

    add more to colors

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -4,7 +4,10 @@ function ColorMyPencils(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none", ctermbg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none", ctermbg = "none" })
+    vim.api.nvim_set_hl(0, "DiffviewStatusAdded", { bg = "none", ctermbg = "blue" })
 end
 
 ColorMyPencils()
+
+vim.cmd("autocmd User Fugitive command! -buffer -bar Gmylog exe 'terminal' FugitivePrepare(['log', '--oneline', '--decorate', '--graph', '--all'])")
 
