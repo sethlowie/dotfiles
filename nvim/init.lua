347d55d04acd2ca7e000c9a7467694b55eacfc68
Seth Lowie <seth.lowie@gmail.com>
2024-04-06 18:53:29 -0400

    checking in latest

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,22 @@
-require("settings")
+local newconfig = os.getenv("TRY_NEW_CONFIG")
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+if newconfig then
+	print("waffles are great")
+else
+	require("settings")
+
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not (vim.uv or vim.loop).fs_stat(lazypath) then
+		vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/folke/lazy.nvim.git",
+			"--branch=stable", -- latest stable release
+			lazypath,
+		})
+	end
+	vim.opt.rtp:prepend(lazypath)
+
+	require("lazy").setup("plugins")
 end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
--- terminal/shortcuts.zsh
+++ terminal/shortcuts.zsh
@@ -2,3 +2,5 @@ alias cf="cd ~/go/src/github.com/sethlowie/dotfiles && nvim"
 alias nlogs="cd ~/.local/state/nvim && nvim"
 
 alias pt="pnpm typecheck"
+alias ptt="pnpm test"
+alias pd="pnpm dev"
